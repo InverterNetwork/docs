84497f8e76d438e5ec5895b8edc1560214df4767
mguleryuz <mguleryuz3@gmail.com>
2024-05-02 18:44:05 +0300

    fix: remove docusaurus cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/anon/Desktop/inverter/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/anon/Desktop/inverter/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/anon/Desktop/inverter/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/anon/Desktop/inverter/docs/src/css/custom.css"),
-];
